eafd9e7df4f61eeb084846ee491ec19305de7d8e
roman <romanus902@gmail.com>
2022-08-07 21:08:22 +0800

    fix app

--- app.py
+++ app.py
@@ -86,7 +86,8 @@ def choose_hero():
         weapon_name = request.form["weapon"]
         unit_class = request.form["unit_class"]
         player = PlayerUnit(name=name, unit_class=unit_classes.get(unit_class),
-                            weapon=unit_classes.get(unit_class), armor=unit_classes.get(unit_class))
+                            weapon=equipment.get_weapon(request.form.get('weapon')),
+                            armor=equipment.get_armor(request.form.get('armor')))
         player.equip_weapon(equipment.get_weapon(weapon_name))
         player.equip_armor(equipment.get_armor(armor_name))
         heroes["player"] = player
@@ -111,7 +112,8 @@ def choose_enemy():
         weapon_name = request.form["weapon"]
         unit_class = request.form["unit_class"]
         enemy = EnemyUnit(name=name, unit_class=unit_classes.get(unit_class),
-                          weapon=unit_classes.get(unit_class), armor=unit_classes.get(unit_class))
+                          weapon=equipment.get_weapon(request.form.get('weapon')),
+                          armor=equipment.get_armor(request.form.get('armor')))
         enemy.equip_weapon(equipment.get_weapon(weapon_name))
         enemy.equip_armor(equipment.get_armor(armor_name))
         heroes["enemy"] = enemy
